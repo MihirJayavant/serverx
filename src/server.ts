c7f49d735d61c53fd811eaf0fe42c14700e25288
mihir jayavant <mihir.jayavant2@gmail.com>
2023-08-11 19:55:32 +0530

    init

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+
+export class Server {
+  app: express.Express
+
+  constructor() {
+    this.app = express()
+  }
+
+  start(port: number) {
+    this.app.listen(port, () => {
+      console.log(`Server is running on port ${port}`)
+    })
+  }
+}

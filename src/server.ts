43ca280465a32806b7337625a53294e12d41a5d1
Mihir Jayavant <mihir.jayavant2@gmail.com>
2024-08-25 20:43:12 +0530

    swagger ui part 1

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,25 @@
 import { Hono } from "@hono/hono";
 import { Router } from "./router.ts";
+import { swaggerUI } from "./open-api/open-api.ts";
 
 export class Server {
     #router = new Hono();
 
     addRouter(router: Router) {
         this.#router.route("/", router.nativeRouter);
+    }
+
+    addOpenApi() {
+        this.#router.get("/doc", (c) => {
+            return c.json({
+                openapi: "3.0.0",
+                info: {
+                    version: "1.0.0",
+                    title: "My API",
+                },
+            });
+        });
+        this.#router.get("/api-docs", swaggerUI({ url: "/doc" }));
     }
 
     serve(options: Deno.ServeOptions) {

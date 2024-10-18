676f0e09988d17bb71c42cf9f14a9df696517abd
Mihir Jayavant <mihir.jayavant2@gmail.com>
2024-10-18 12:28:26 +0530

    fmt fixes

--- src/http/result.spec.ts
+++ src/http/result.spec.ts
@@ -2,19 +2,19 @@ import { assertEquals } from "jsr:@std/assert";
 import { isError, isSuccess, statusCodes } from "./result.ts";
 
 Deno.test("isSuccess", () => {
-    const successResult = {
-        status: statusCodes.Ok,
-        data: {
-            name: "John Doe",
-        },
-    };
-    assertEquals(isSuccess(successResult), true);
+  const successResult = {
+    status: statusCodes.Ok,
+    data: {
+      name: "John Doe",
+    },
+  };
+  assertEquals(isSuccess(successResult), true);
 });
 
 Deno.test("isError", () => {
-    const errorResult = {
-        status: statusCodes.InternalServerError,
-        error: "Internal Server Error",
-    };
-    assertEquals(isError(errorResult), true);
+  const errorResult = {
+    status: statusCodes.InternalServerError,
+    error: "Internal Server Error",
+  };
+  assertEquals(isError(errorResult), true);
 });

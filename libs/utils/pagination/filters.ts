72b7b894e622b52914bd067fb1d21457392677fb
Mihir Jayavant <mihir.jayavant2@gmail.com>
2025-02-28 17:28:08 +0530

    selector added

--- libs/utils/pagination/filters.ts
+++ libs/utils/pagination/filters.ts
@@ -26,3 +26,28 @@ export type FilterValueType = string | string[];
 export type FilteringParams = {
   [key: string]: FilterValueType;
 };
+
+/**
+ * Selector parameters
+ */
+export type SelectorParams<T> = Array<keyof T>;
+
+/**
+ * Selector type to be used with external databse libraries like Mongoose, TypeORM, Drizzle, etc.
+ */
+export type Selector<T> = Partial<Record<keyof T, 1>>;
+
+/**
+ * Convert filtering parameters to a format that can be used with external database libraries
+ * @param selector - Selector parameters
+ * @returns - Selector object that can be used with external database libraries
+ */
+export function convertSelector<T>(
+  selector: SelectorParams<T>,
+): Selector<T> {
+  const result: Selector<T> = {};
+  for (const key of selector) {
+    result[key] = 1;
+  }
+  return result;
+}
